fd2aac944da8c0684af6b8feb95c44fa141a9359
Netesfiu <68812986+Netesfiu@users.noreply.github.com>
2020-08-19 21:05:06 +0200

    Update build.sql

--- data/db/build.sql
+++ data/db/build.sql
@@ -1,13 +1,7 @@
 CREATE TABLE IF NOT EXISTS guilds (
-	GuildID integer PRIMARY KEY,
+	ServerID integer PRIMARY KEY,
+	ServerName text DEFAULT NONE,
 	Prefix text DEFAULT "//"
-);
-
-CREATE TABLE IF NOT EXISTS exp (
-	UserID integer PRIMARY KEY,
-	XP integer DEFAULT 0,
-	Level integer DEFAULT 0,
-	XPLock text DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS mutes (
@@ -16,15 +10,9 @@ CREATE TABLE IF NOT EXISTS mutes (
 	EndTime text
 );
 
-CREATE TABLE IF NOT EXISTS starboard (
-	RootMessageID integer PRIMARY KEY,
-	StarMessageID integer,
-	Stars integer DEFAULT 1
-);
-
 CREATE TABLE IF NOT EXISTS hallgato (
 	UserID integer PRIMARY KEY,
-    NEPTUN integer DEFAULT 0,
+    NEPTUN text DEFAULT "Nem Regisztrált",
 	Kollegium integer DEFAULT 0,
 	Kozossegi integer DEFAULT 1
 );
